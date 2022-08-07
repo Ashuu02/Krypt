83146b637a5e264576b85a777df1f18437c935ba
Ashuu02 <ashushimpi02@gmail.com>
2022-08-08 00:31:51 +0530

    service file updated

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,3 +1,8 @@
+import { useContext, React } from "react";
+import { TransactionContext } from "../context/TransactionContext";
+import dummyData from "../../utils/dummyData";
+
+
 const Transactions =()=>{
     return(
         <h1>Transactions</h1>
